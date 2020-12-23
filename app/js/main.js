6a4d440b91738eaaf5e6e61ccffb4a597d88cce4
kolweb-dev <kolya.tkachukki@gmail.com>
2020-12-23 13:57:08 +0200

    start project

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const root = document.querySelector('.todoapp');
+
+const newToDoField = document.querySelector('.new-todo');
+
+newToDoField.addEventListener('keydown', (event) => {
+
+    if (event.key !== 'Enter') {
+        return;
+    }
+    if (!newToDoField.value){
+        return ;
+    }
+
+
+
+    const itemsList = root.querySelector('.todo-list');
+    const id = +new Date();
+
+    itemsList.insertAdjacentHTML('beforeend', `<li>
+    <input id="todo-${id}" class="toggle" type="checkbox">
+    <label for="todo-${id}">${newToDoField.value}</label>
+    <button class="destroy"></button>
+    </li>`);
+    newToDoField.value = '';
+})
