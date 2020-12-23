dac619bae6ce8dfcd69ad81275e322ebe99626c2
kolweb-dev <kolya.tkachukki@gmail.com>
2020-12-23 17:56:53 +0200

    Togle all

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,24 @@
 const root = document.querySelector('.todoapp');
 const newToDoField = document.querySelector('.new-todo');
 const itemsList = root.querySelector('.todo-list');
+const allToggler = root.querySelector('.toggle-all');
 
 function updateInfo() {
     const notCompletedTogglers = root.querySelectorAll('.toggle:not(:checked)');
     const counter = root.querySelector('.todo-count');
     counter.innerHTML = `${notCompletedTogglers.length} item left`;
+
+    allToggler.checked = notCompletedTogglers.length === 0;
 }
+allToggler.addEventListener('change', () => {
+    const togglers = root.querySelectorAll('.toggle');
+
+        for (const toggler of togglers){
+            toggler.checked = allToggler.checked;
+            toggler.closest('.todo-item').classList.toggle('completed',allToggler.checked)
+        }
+
+})
 
 newToDoField.addEventListener('keydown', (event) => {
 
@@ -45,6 +57,6 @@ itemsList.addEventListener('click', (event) => {
         return;
     }
 
-    event.target.closest('.todo-item').classList.toggle('completed', event.target.checked);
+    event.target.c.toggle('completed', event.target.checked);
     updateInfo();
 })
