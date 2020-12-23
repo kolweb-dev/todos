3f3d74bb2d8b0f010ad5051cdb941ed48431b353
kolweb-dev <kolya.tkachukki@gmail.com>
2020-12-23 18:55:01 +0200

    Cleat completed

--- app/js/main.js
+++ app/js/main.js
@@ -4,21 +4,34 @@ const root = document.querySelector('.todoapp');
 const newToDoField = document.querySelector('.new-todo');
 const itemsList = root.querySelector('.todo-list');
 const allToggler = root.querySelector('.toggle-all');
+const clearCompletedButton = root.querySelector('.clear-completed')
 
 function updateInfo() {
+    const completedTogglers = root.querySelectorAll('.toggle:checked');
     const notCompletedTogglers = root.querySelectorAll('.toggle:not(:checked)');
     const counter = root.querySelector('.todo-count');
-    counter.innerHTML = `${notCompletedTogglers.length} item left`;
 
+    counter.innerHTML = `${notCompletedTogglers.length} items left`;
     allToggler.checked = notCompletedTogglers.length === 0;
+    clearCompletedButton.hidden = completedTogglers.length == 0;
 }
+clearCompletedButton.addEventListener('click', ()=>{
+    const completedTogglers = root.querySelectorAll('.toggle:checked');
+
+    for (const toggler of completedTogglers){
+        toggler.closest('.todo-item').remove();
+    }
+    updateInfo();
+});
+
+
 allToggler.addEventListener('change', () => {
     const togglers = root.querySelectorAll('.toggle');
 
-        for (const toggler of togglers){
-            toggler.checked = allToggler.checked;
-            toggler.closest('.todo-item').classList.toggle('completed',allToggler.checked)
-        }
+    for (const toggler of togglers) {
+        toggler.checked = allToggler.checked;
+        toggler.closest('.todo-item').classList.toggle('completed', allToggler.checked)
+    }
 
 })
 
@@ -52,11 +65,11 @@ itemsList.addEventListener('click', (event) => {
     updateInfo();
 })
 
-itemsList.addEventListener('click', (event) => {
+itemsList.addEventListener('change', (event) => {
     if (!event.target.matches('.toggle')) {
         return;
     }
 
-    event.target.c.toggle('completed', event.target.checked);
+    event.target.closest('.todo-item').classList.toggle('completed', event.target.checked);
     updateInfo();
 })
