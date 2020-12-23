7171ae8ac8ecc13483ba1c1bc542f73f8e9a71dc
kolweb-dev <kolya.tkachukki@gmail.com>
2020-12-23 14:55:07 +0200

    Remove todo

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,41 @@
 'use strict';
 
 const root = document.querySelector('.todoapp');
-
 const newToDoField = document.querySelector('.new-todo');
+const itemsList = root.querySelector('.todo-list');
+
+function updateInfo() {
+    const notCompletedTogglers = root.querySelectorAll('.toggle:not(:checked)');
+    const counter = root.querySelector('.todo-count');
+    counter.innerHTML = `${notCompletedTogglers.length} item left`;
+}
 
 newToDoField.addEventListener('keydown', (event) => {
 
     if (event.key !== 'Enter') {
         return;
     }
-    if (!newToDoField.value){
-        return ;
+    if (!newToDoField.value) {
+        return;
     }
 
-
-
-    const itemsList = root.querySelector('.todo-list');
     const id = +new Date();
 
-    itemsList.insertAdjacentHTML('beforeend', `<li>
+    itemsList.insertAdjacentHTML('beforeend', `<li class="todo-item">
     <input id="todo-${id}" class="toggle" type="checkbox">
     <label for="todo-${id}">${newToDoField.value}</label>
     <button class="destroy"></button>
     </li>`);
     newToDoField.value = '';
 
-    const notCompletedTogglers = root.querySelectorAll('.toggle:not(:checked)');
-    const counter = root.querySelector('.todo-count');
+    updateInfo();
+})
 
-    counter.innerHTML = `${notCompletedTogglers.length} item left`;
+itemsList.addEventListener('click', (event) => {
+    if (!event.target.matches('.destroy')) {
+        return;
+    }
+
+    event.target.closest('.todo-item').remove();
+    updateInfo();
 })
